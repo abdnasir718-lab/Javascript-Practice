329ece51f5e1424adf1d5481ea072ac9dfeee3ef
Abdullah Nasir <abdnasir718@gmail.com>
2026-03-14 02:34:40 +0500

    Updated

--- Project (Name Generator)/script.js
+++ Project (Name Generator)/script.js
@@ -1,8 +1,8 @@
 let firstName = "Abdullah";
 let lastName = "Nasir";
 
-let fullName = `My first name is ${firstName} and last name is ${lastName}
+let FullName = `My first name is ${firstName} and last name is ${lastName}
 Length of firstname is ${firstName.length}
 Length of lastname is ${lastName.length}`;
 
-console.log(fullName);
+console.log(FullName);
