2fe3e031cdfe254f30a234af477495727d0eb984
Abdullah Nasir <abdnasir718@gmail.com>
2026-03-14 23:49:03 +0500

    Learned arrays today

---
+++ Arrays/script.js
@@ -0,0 +1,30 @@
+//Arrays 
+let ourArray= ["Abdullah",25];
+
+//Nested arrays
+let nestedArr=[["Quincy",25],["Zoro",30]];
+
+//Accessing array data
+let Arr= [10,20,30];
+let data= Arr[1];
+console.log(data)
+
+//Changing the value of the array using index
+let ourArrays=[10,20,30,40];
+ourArrays[2]=45;
+console.log(ourArrays)
+
+//2D arrays
+let matrix=[[1,2,3],[4,5,6],[7,8,9]];
+let da=matrix[1][2];
+console.log(da)
+ //Push() Adds element to the end of the array
+ let ourArrs=["Stimpson",10];
+ console.log(ourArrs)
+ ourArrs.push(["happy","joy"]);
+ console.log(ourArrs)
+
+ //Pop() Remove LAst elemnet
+ let mat=[1,2,3];
+ mat.pop()
+ console.log(mat)
